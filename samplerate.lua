baf96881ae7f992eb35a0407462373cc26a0b313
sonoro1234 <sonoro@telefonica.net>
2019-04-29 11:09:26 +0200

    ffi load samplerate-0

--- samplerate.lua
+++ samplerate.lua
@@ -163,6 +163,6 @@ void src_int_to_float_array (const int *in, float *out, int len) ;
 void src_float_to_int_array (const float *in, int *out, int len) ;
 ]]
 
-local lib = ffi.load"samplerate"
+local lib = ffi.load"samplerate-0"
 
 return lib
